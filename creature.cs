6e9d6be8152de7950a49e9686674ae02b8b2d221
HaiMy2107 <fregstegen001@gmail.com>
2024-09-07 11:50:36 +0700

    Create creature.cs

---
+++ creature.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ConsoleFeatures
+{
+  class Program
+  {
+    static void Main ()
+    {
+      Console.WriteLine(" .-.");
+      
+      Console.WriteLine("(o o)");
+      
+      Console.WriteLine("| 0 |");
+      
+      Console.WriteLine("|   |");
+      
+      Console.WriteLine("'~~~'");
+
+    }
+  }
+}
